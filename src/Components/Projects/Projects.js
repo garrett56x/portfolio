e9bc77d44f99b50bf11b16b60a4bd583a5c2ccfd
Garrett <garrett56x@gmail.com>
2020-10-26 14:39:36 -0700

    Updated project cards

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,32 +1,83 @@
 import React from 'react';
 import './Projects.css';
-import { Paper, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { Card, CardActionArea, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
 import paintconnection from '../../assets/paintconnection.png';
 import weddingsite from '../../assets/weddingsite.png';
 
+const useStyles = makeStyles({
+    projectsContainer: {
+        display: "flex",
+        justifyContent: "space-around",
+        flexFlow: "row wrap",
+    },
+    project: {
+      maxWidth: 345,
+      margin: "0 10px",
+      marginBottom: "20px",
+    },
+    media: {
+      height: 140,
+    },
+  });
+
 function Projects() {
+    const classes = useStyles();
+
   return (
     <div className="projects">
         <Typography variant="h4" gutterBottom>Projects</Typography>
-        <div className="projects-container">
-            <a className="project-link" href="https://www.thepaintconnection.com/" target="_blank">
-                <Paper className="project">
-                    <div className="project-image" style={{ backgroundImage: `url(${paintconnection})` }} />
-                    <div className="project-description">
-                        <Typography variant="h5">The Paint Connection</Typography>
-                        <Typography variant="body2">A lead marketplace for painting houses built using React &amp; Redux with a Node &amp; Express REST API</Typography>
-                    </div>
-                </Paper>
-            </a>
-            <a className="project-link" href="https://www.angieandgarrett.com/" target="_blank">
-                <Paper className="project">
-                    <div className="project-image" style={{ backgroundImage: `url(${weddingsite})` }} />
-                    <div className="project-description">
-                        <Typography variant="h5">Wedding Website</Typography>
-                        <Typography variant="body2">A website for my wedding built using React</Typography>
-                    </div>
-                </Paper>
-            </a>
+        <div className={classes.projectsContainer}>
+            <Card className={classes.project}>
+                <CardActionArea href="https://www.thepaintconnection.com/" target="_blank">
+                    <CardMedia
+                    className={classes.media}
+                    image={paintconnection}
+                    title="The Paint Connection"
+                    />
+                    <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        The Paint Connection
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        A lead marketplace for painting houses built using React &amp; Redux with a Node &amp; Express REST API
+                    </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button size="small" color="primary" href="https://github.com/garrett56x/painting-leads-frontend" target="_blank">
+                        Frontend Repo
+                    </Button>
+                    <Button size="small" color="primary" href="https://github.com/garrett56x/painting-leads-api" target="_blank">
+                        Backend Repo
+                    </Button>
+                </CardActions>
+            </Card>
+            <Card className={classes.project}>
+                <CardActionArea href="https://www.angieandgarrett.com/" target="_blank">
+                    <CardMedia
+                    className={classes.media}
+                    image={weddingsite}
+                    title="Wedding Website"
+                    />
+                    <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        Wedding Website
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        A website for my wedding built using React. Highlights: Favoriting and filtering on Things to Do page.
+                    </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button size="small" color="primary" href="https://www.angieandgarrett.com/things-to-do" target="_blank">
+                        Things To Do
+                    </Button>
+                    <Button size="small" color="primary" href="https://github.com/garrett56x/wedding-site" target="_blank">
+                        Github Repo
+                    </Button>
+                </CardActions>
+            </Card>
             <div className="stats">
                 <div className="stats-left">
                     <img className="stat" src="https://github-readme-stats.vercel.app/api?username=garrett56x&show_icons=true&count_private=true&hide=issues,contribs&theme=dark" alt="github-stats" />
